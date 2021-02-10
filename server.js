c6003330416c98b364f9b42f21c8482376c42f1f
jlb1999 <jlbroughton88@gmail.com>
2021-02-10 16:25:10 -0500

    2nd commit

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 let express = require("express");
-let path = require("path")
+let path = require("path");
+let cors = require("cors");
+let bodyParser = require("body-parser")
 let app = express();
-let port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
+app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/api", (req, res) => {
     res.send("Hello from server")
 })
 
@@ -13,7 +26,8 @@ if (process.env.NODE_ENV === "production") {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
   }
-
+  
+let port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server listening at: ${port}`)
 })
